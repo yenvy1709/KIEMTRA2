8214cd2feab59d61aeaf04ab57eeb7259dc368fb
tinko0o <tinhneko2002@gmail.com>
2024-03-15 10:07:01 +0700

    Revert "them label username,textbox username"
    This reverts commit fbc9ce2a13b23dfeb64b354a3d9d1496104bf1f0.

--- login.php
+++ login.php
@@ -1,41 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <title>Document</title>
 </head>
-
 <body>
     <div class="container">
         <div class="row justify-content-center mt-5">
             <div class="col-md-6">
                 <form action="result.php" method="POST" class="mt-4">
                     <div class="form-group">
-                        <label for="username">Ten dang nhap:</label>
+                        <label for="username">UserName:</label>
                         <input type="text" class="form-control" id="username" name="username" required>
                     </div>
-                    <div class="form-group">
-                        <label for="password">Mat khau:</label>
-                        <input type="password" class="form-control" id="password" name="password" required>
-                    </div>
-                    <button type="submit" class="btn btn-primary btn-block">Dang nhap</button>
                 </form>
             </div>
         </div>
     </div>
-    <?php
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-        if ($username == 'admin' && $password == 'admin') {
-            echo "<span style='color:purple; font-weight:bold;'>Dang nhap thanh cong</span> ";
-        } else {
-            echo "<span style='color:red; font-weight:bold;'>Tên däng nhâp/Mât khau sai. Vui long nhâp lai </span> ";
-        }
-?>
 </body>
-
 </html>
